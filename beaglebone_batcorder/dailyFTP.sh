1231859c84e9fba2aa98cc66429f3c6bfef05889
Roeland Develter <56031454+RoelandDevelter@users.noreply.github.com>
2019-10-18 16:50:47 +0200

    Create dailyFTP.sh
    TODO: comment

---
+++ beaglebone_batcorder/dailyFTP.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+while getopts 'd' opt; do
+case "$opt" in
+d) flag=1 ;;
+*) flag=0 ;;
+esac
+done
+
+logs="/home/debian/logs"
+logfile="${logs}/BBB.log"
+bbb_sd="/media/sd/"
+bc_sd="/media/usb-drive/"
+un="yourusernamehere"
+pwd="yourpasswordhere"
+ftp_server="ftp.yourwebsite.be"
+
+echo >> $logfile # append empty line in log
+echo "----------------------------------------"  >> $logfile
+date >> $logfile # append date and time
+
+mount /dev/sda1 $bc_sd &>> $logfile # mount the batcorder
+mount /dev/mmcblk0p1 $bbb_sd # mount the BBB's SD card
+
+rsync -av $bc_sd $bbb_sd &>> $logfile
+
+#transfer files using FTP
+if [ "$flag" = 1 ]; then
+/usr/bin/lftp -e "set net:timeout 30; mirror --Remove-source-dirs -R ${bbb_sd} ${target_dir_files}; bye" -u $un,$pwd $ftp_server &>> $logfile
+else
+/usr/bin/lftp -e "set net:timeout 30; mirror -R ${bbb_sd} ${target_dir_files}; bye" -u $un,$pwd $ftp_server &>> $logfile
+fi
+/usr/bin/lftp -e "set net:timeout 30; mirror -R ${logs} ${target_dir_logs}; bye" -u $un,$pwd $ftp_server
+
+umount /media/sd/
+umount /media/usb-drive/
+
+if [ "$flag" = 1 ]; then
+echo "relabeling" >> $logfile
+mlabel -i /dev/sda1 -s :: DELETEME
+fi
